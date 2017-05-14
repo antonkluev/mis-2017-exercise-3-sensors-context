338c405f7a77d109d34a27c9153d157531ad4839
antonkluev <dev@antonkluev.com>
2017-05-15 00:38:43 +0200

    layout is done

--- app/src/main/java/com/uni/antonkluev/app/CanvasView.java
+++ app/src/main/java/com/uni/antonkluev/app/CanvasView.java
@@ -1,10 +1,11 @@
 
 package com.uni.antonkluev.app;
 
+import android.graphics.Color;
+import android.util.Log;
 import android.view.View;
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.util.AttributeSet;
@@ -13,55 +14,77 @@ import java.util.ArrayList;
 
 // https://examples.javacodegeeks.com/android/core/graphics/canvas-graphics/android-canvas-example/
 public class CanvasView extends View {
-    private Path  mPath;
-    private Paint mPaint;
-
+    private int width;
+    private int height;
+    private int sampleSize = 100;
+    private int padding = 10;
+    public ArrayList <Axis> axis = new ArrayList<Axis>();
+    // methods
     public CanvasView(Context c, AttributeSet attrs) {
         super(c, attrs);
-        mPaint = new Paint();
-        mPaint.setAntiAlias(true);
-        mPaint.setStyle(Paint.Style.STROKE);
-        mPaint.setStrokeJoin(Paint.Join.ROUND);
-        mPaint.setStrokeWidth(1f);
-        mPath  = new Path();
+        axis.add(new Axis(Color.RED));
+        axis.add(new Axis(Color.GREEN));
+        axis.add(new Axis(Color.BLUE));
+        axis.add(new Axis(Color.GRAY));
     }
-    // override onDraw
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        // draw text
-        drawString(canvas, "jesus christ", 0, 0);
-
-        ArrayList<Float> position = new ArrayList<Float>();
-        position.add(150f);
-        position.add(150f);
-        position.add(0f);
-        position.add(0f);
-        drawLine(canvas, position, Color.LTGRAY);
-
-        ArrayList<Float> position1 = new ArrayList<Float>();
-        position1.add(0f);
-        position1.add(0f);
-        position1.add(150f);
-        position1.add(150f);
-        drawLine(canvas, position1, Color.RED);
+        // print plane
+        drawCoordinatePlane(canvas);
+        // print axis
+        for (int i = 0; i < axis.size(); i ++)
+            canvas.drawPath(axis.get(i).path, axis.get(i).paint);
     }
-    void drawString (Canvas canvas, String text, int x, int y) {
-        Paint textPaint = new Paint();
-        canvas.drawText(
-                text,
-                x - textPaint.measureText(text) / 2,
-                y - (textPaint.descent() + textPaint.ascent()) / 2,
-                textPaint);
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        width  = w;
+        height = h;
+        super.onSizeChanged(w, h, oldw, oldh);
     }
-    void drawLine (Canvas canvas, ArrayList<Float> position, int color) {
-        mPaint.setColor(color);
-        float width  = (float)canvas.getWidth() / (position.size() - 1f);
-        float height = (float)canvas.getHeight();
-        mPath.moveTo(0, height - position.get(0));
-        for (int i = 1; i < position.size(); i ++)
-            mPath.lineTo((float)i * width, height - position.get(i));
-        canvas.drawPath(mPath, mPaint);
-        mPath.reset();
+    void drawCoordinatePlane (Canvas canvas) {
+        // draw line
+        Path  path  = new Path();
+        Paint paint = new Paint();
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setColor(Color.LTGRAY);
+        int size = 5;
+        float x = width - 2 * padding;
+        float h  = (height - padding * 2) / (size - 1);
+        for (int i = 0; i < size; i ++) {
+            // draw numbers
+            String text = String.valueOf(i);
+            float y = height - padding - i * h;
+            Paint tp = new Paint();
+            canvas.drawText(text,
+                    x - tp.measureText(text) / 2,
+                    y - (tp.descent() + tp.ascent()) / 2, tp);
+            // draw line
+            path.moveTo(0, y);
+            path.lineTo(width - 3 * padding, y);
+            canvas.drawPath(path, paint);
+        }
+    }
+    public class Axis {
+        private ArrayList<Float> data = new ArrayList<Float>(); // gray
+        public Path  path  = new Path();
+        public Paint paint = new Paint();
+        // methods
+        public Axis (int color) {
+            paint.setStyle(Paint.Style.STROKE);
+            paint.setColor(color);
+        }
+        public void addPoint (float value) {
+            data.add(value);
+            if (data.size() > sampleSize) data.remove(0);
+            if (data.size() > 0) {
+                path.reset();
+                float w = width / (data.size() - 1f);
+                path.moveTo(0, height - padding - data.get(0));
+                for (int i = 1; i < data.size(); i ++)
+                    path.lineTo((float)i * w, height - padding - data.get(i));
+                postInvalidate();
+            }
+        }
     }
 }
--- app/src/main/java/com/uni/antonkluev/app/MainActivity.java
+++ app/src/main/java/com/uni/antonkluev/app/MainActivity.java
@@ -1,17 +1,54 @@
 package com.uni.antonkluev.app;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.util.Log;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     private CanvasView customCanvas;
+    private SeekBar    rateSeekBar;
+    private SeekBar    windowSizeSeekBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rateSeekBar = (SeekBar) findViewById(R.id.rateSeekBar);
+        windowSizeSeekBar = (SeekBar) findViewById(R.id.windowSizeSeekBar);
         customCanvas = (CanvasView) findViewById(R.id.accelerometerCanvasView);
+
+        rateSeekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress,boolean fromUser) {
+                customCanvas.axis.get(0).addPoint(progress);
+            }
+        });
+
+        windowSizeSeekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {}
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress,boolean fromUser) {
+                customCanvas.axis.get(1).addPoint(progress);
+            }
+        });
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
     }
 }
